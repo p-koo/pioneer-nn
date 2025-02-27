493c597078d6a875f656073065148348e9773a3f
koo-lab <koolaboratory.gmail.com>
2025-02-26 22:53:59 -0500

    updates

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,11 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # For Google/NumPy style docstrings
+    # other extensions...
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
