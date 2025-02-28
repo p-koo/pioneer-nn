dce12254c66ee90795cddab222ac61fb4b1319c2
alescrnjar <a.crnjar@gmail.com>
2025-02-28 18:40:06 -0500

    upsample

--- utils/utils.py
+++ utils/utils.py
@@ -1,27 +1,28 @@
-class UpSelector(Selector):
-    """Selector that upsamples sequences by repeating them.
+import torch
+
+def upsample(x: torch.Tensor, min_needed: int = 0, repeats: Optional[int] = None) -> torch.Tensor:
+    """Upsample sequences by repeating them.
     
-    Attributes:
+    Args:
+        x: Input tensor to upsample
         min_needed: Minimum number of sequences needed
         repeats: Number of times to repeat sequences. If None, calculated from min_needed.
+        
+    Returns:
+        torch.Tensor of upsampled sequences
     """
-    def __init__(self, min_needed:int=0, repeats:Optional[int]=None) -> None:
-        super().__init__(min_needed, None)
-        self.repeats = repeats
-
-    def make_idx(self, x:Union[np.ndarray,torch.Tensor]) -> np.ndarray:
-        """Generate indices that repeat the input sequences.
-        
-        Args:
-            x: Input array to select from
-            
-        Returns:
-            numpy.ndarray of indices that repeat the sequences
-        """
-        if self.repeats is None:
-            upsamples = ceil(self.n_to_get/len(x))
-        else:
-            upsamples = self.repeats
-        
-        idx = np.concatenate([np.arange(len(x)) for i in range(upsamples)])
-        return idx
+    # Convert input to tensor if it's not already
+    if not isinstance(x, torch.Tensor):
+        x = torch.tensor(x)
+    
+    # Calculate number of repeats needed
+    if repeats is None:
+        upsamples = ceil(min_needed / len(x))
+    else:
+        upsamples = repeats
+    
+    # Create indices for upsampling
+    idx = torch.arange(len(x)).repeat(upsamples)
+    
+    # Return upsampled tensor
+    return x[idx]
